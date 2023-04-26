442f268a2600db1f9773af173fa189851d758b6e
Ryan J. Gray <ryan@ryanjgray.com>
2023-04-26 23:51:10 +0100

    feat: reversed ReflectionMap quality enum.

---
+++ Source/engine/include/ReflectionMap.h
@@ -0,0 +1,17 @@
+/**
+ * The OpenIsolation Project
+ */
+
+#pragma once
+
+enum eReflectionMapQuality {
+  // Disables the reflection renderer entirely.
+  REFLECTION_MAP_QUALITY_OFF,
+  // This state is implicit, there's no code that searches for it.
+  // As long as the state is not 0 (OFF), it'll activate the reflection renderer, just with no mip-maps.
+  REFLECTION_MAP_QUALITY_ON,
+  // Renders a low-res blur on the reflection mip-maps.
+  REFLECTION_MAP_QUALITY_LOW_RES,
+  // [PC Default] Renders a high-res gaussian blur on the reflection mip-maps.
+  REFLECTION_MAP_QUALITY_HIGH_RES,
+};
---
+++ Source/engine/source/ReflectionMap.cpp
@@ -0,0 +1,6 @@
+/**
+ * The OpenIsolation Project
+ */
+
+#include "ReflectionMap.h"
+
