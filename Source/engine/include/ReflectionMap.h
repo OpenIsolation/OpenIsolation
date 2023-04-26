39faf33855e835780e7bd3ab5794045979fa535f
Ryan J. Gray <ryan@ryanjgray.com>
2023-04-26 23:55:56 +0100

    doc: update ReflectionMap enum comment.

--- Source/engine/include/ReflectionMap.h
+++ Source/engine/include/ReflectionMap.h
@@ -7,8 +7,7 @@
 enum eReflectionMapQuality {
   // Disables the reflection renderer entirely.
   REFLECTION_MAP_QUALITY_OFF,
-  // This state is implicit, there's no code that searches for it.
-  // As long as the state is not 0 (OFF), it'll activate the reflection renderer, just with no mip-maps.
+  // Implicit state. As long as the state is not 0 (OFF), it'll activate the reflection renderer, just with no mip-maps.
   REFLECTION_MAP_QUALITY_ON,
   // Renders a low-res blur on the reflection mip-maps.
   REFLECTION_MAP_QUALITY_LOW_RES,
