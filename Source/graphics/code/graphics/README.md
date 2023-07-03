d8a9952a76d53f37b2123f8db5665ebab8b1d6b1
Ryan J. Gray <ryan@ryanjgray.com>
2023-07-04 00:55:41 +0100

    feat: Graphics - add some initial work for more enums, and GfxSamplerStateDescr.

--- Source/graphics/code/graphics/README.md
+++ Source/graphics/code/graphics/README.md
@@ -5,12 +5,14 @@ Alien: Isolation's renderer supported multiple different graphics APIs.
 ### Known supported graphics APIs
 | Platform      | Graphics API             |
 |---------------|--------------------------|
-| Windows       | DX11                     |
+| Windows       | DX11[^1]                 |
 | Linux         | DX11 (Wrapped to OpenGL) |
 | Mac           | DX11 (Wrapped to OpenGL) |
 | iOS           | DX11 (Wrapped to Metal)  |
 | Android       | DX11 (Wrapped to OpenGL) |
 | PlayStation 4 | GNM                      |
+[^1]: There is logic in this library to override some settings (such as Depth of Field) for non-DX11 devices.
+  It's possible DX10 support was planned at one point, as there is a _feature level_ enumeration entry for it.
 
 Feral Interactive opted for using an in-house DirectX wrapper library called *inDirectX*, to save having to write new
 GFX API code.
--- Source/graphics/code/graphics/gfx_api.h
+++ Source/graphics/code/graphics/gfx_api.h
@@ -7,6 +7,8 @@
 #include <cstdint>
 #include <cassert>
 
+static constexpr std::uint32_t GRAPHICS_UNKNOWN_VALUE = 0x3f18fff7;
+
 template<typename T>
 struct GfxHandle {
   T* handle;
@@ -15,6 +17,13 @@ struct GfxHandle {
 template<typename T>
 struct GfxEnum {
   T value;
+};
+
+enum eGfxFeatureLevel {
+  FEATURE_LEVEL_UNKNOWN,
+  FEATURE_LEVEL_D3D10,
+  FEATURE_LEVEL_D3D11,
+  FEATURE_LEVEL_UNSUPPORTED,
 };
 
 enum eGfxUsage {
@@ -34,9 +43,22 @@ enum eGfxGPUIndexBufferFormat {
 };
 
 enum eGfxQueryType {
-  QUERY_TYPE_UNKNOWN,
+  QUERY_TYPE_EVENT,
   QUERY_TYPE_OCCLUSION,
   QUERY_TYPE_TIMESTAMP_DISJOINT = 3,
+  QUERY_TYPE_UNKNOWN_VALUE = GRAPHICS_UNKNOWN_VALUE,
+};
+
+enum eGfxRenderStateOptionsCompare {
+  RENDER_STATE_OPTIONS_COMPARE_UNKNOWN_VALUE = GRAPHICS_UNKNOWN_VALUE,
+};
+
+enum eGfxSamplerStateOptionsAddress {
+  SAMPLER_STATE_OPTIONS_ADDRESS_UNKNOWN_VALUE = GRAPHICS_UNKNOWN_VALUE,
+};
+
+enum eGfxSamplerStateOptionsMaxAnisotropy {
+  SAMPLER_STATE_OPTIONS_MAX_ANISOTROPY_UNKNOWN_VALUE = GRAPHICS_UNKNOWN_VALUE,
 };
 
 enum eGfxShaderType {
@@ -60,3 +82,12 @@ struct GfxQueryDescr {
   GfxEnum<eGfxQueryType> queryType;
 };
 static_assert(sizeof(GfxQueryDescr) == 4, "Invalid size!");
+
+struct GfxSamplerStateDescr {
+  GfxEnum<eGfxSamplerStateOptionsAddress> address1;
+  GfxEnum<eGfxSamplerStateOptionsAddress> address2;
+  GfxEnum<eGfxSamplerStateOptionsAddress> address3;
+  GfxEnum<eGfxRenderStateOptionsCompare> optionsCompare;
+  GfxEnum<eGfxSamplerStateOptionsMaxAnisotropy> maxAnisotropy;
+  // Pretty sure there's more to this struct, but this is all I've worked out so far.
+};
